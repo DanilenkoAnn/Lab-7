6383d4794acb2df9673576e898c45ba63a82d8dd
DanilenkoAnn <anna.danilenko.06@mail.ru>
2025-06-30 02:24:56 +0300

    исправленно по pep 8

--- lab_7_1.py
+++ lab_7_1.py
@@ -3,13 +3,25 @@ import json
 
 
 city_name = 'Kyoto'
-key = 'b0873bc9e25b956902373a672b123374'
-weather = requests.post(f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={key}&lang=ru&units=metric')
+key = '620c7d7ac11663ab4536ac0219607ca9'
+lang = 'ru'
+
+city = requests.get(f'http://api.openweathermap.org/geo/1.0/'
+                    f'zip?zip=600-8216,JP&appid={key}')
+data = city.json()
+lat = data['lat']
+lon = data['lon']
+
+weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?'
+                       f'lat={lat}&lon={lon}&appid={key}&lang={lang}')
 data = weather.json()
 
-weather = data['weather'][0]['description']
+description = data['weather'][0]['description']
 temperature = data['main']['temp']
 humidity = data['main']['humidity']
 pressure = data['main']['pressure']
 
-print(f'В Киото погода: {weather}, температура: {temperature}, влажность: {humidity}, давление: {pressure}')
+print(f'В Киото {description}, температура: {temperature}, '
+      f'влажность: {humidity}, давление: {pressure}')
+
+ 
