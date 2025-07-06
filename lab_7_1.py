6052a9af269c46997cbad29b9eb4716079873c3e
DanilenkoAnn <anna.danilenko.06@mail.ru>
2025-07-06 14:41:04 +0300

    скрыла api

--- lab_7_1.py
+++ lab_7_1.py
@@ -1,11 +1,13 @@
 import requests
 import json
+import os
+from dotenv import load_dotenv, dotenv_values
 
-
+load_dotenv()
+key = os.getenv("KEY")
 city_name = 'Kyoto'
-key = '620c7d7ac11663ab4536ac0219607ca9'
 lang = 'ru'
-
+ 
 city = requests.get(f'http://api.openweathermap.org/geo/1.0/'
                     f'zip?zip=600-8216,JP&appid={key}')
 data = city.json()
@@ -23,5 +25,3 @@ pressure = data['main']['pressure']
 
 print(f'В Киото {description}, температура: {temperature}, '
       f'влажность: {humidity}, давление: {pressure}')
-
- 
